e4b4dd216628e4d4a444a372f39e4b6ef350602d
willian <willz3@outlook.com>
2020-06-09 21:24:58 -0300

    Adjustment in css chat layout

--- web/src/components/chat/index.jsx
+++ web/src/components/chat/index.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import { MdSend } from 'react-icons/md'
 import './styles.css'
 
-const Chat = () => (
-    <div id="container">
-        <ul>
-            <li className="message message-out">
-                <p>
-                    <span className="text-message">Oi aaaaaaaaaaaaaaaaaaa</span> <br />
-                    <span className="hour-message">21:43</span>
-                </p>
-            </li>
-            <li className="message message-in">
-                <p>
-                    <span className="text-message">Oi, tudo bem?</span> <br />
-                    <span className="hour-message">21:43</span>
-                </p>
-            </li>
-        </ul>
-    </div>
-)
+const Chat = () => {
+
+
+
+    return (
+        <div id="chat">
+            <ul class="content">
+                <li className="message message-out">
+                    <p>
+                        <span className="text-message">Hello!</span> <br />
+                        <span className="hour-message">21:43</span>
+                    </p>
+                </li>
+                <li className="message message-in">
+                    <p>
+                        <span className="text-message">Hey, what's up?</span> <br />
+                        <span className="hour-message">21:43</span>
+                    </p>
+                </li>
+            </ul>
+            <form id="form-message" className="end">
+                <input type="text"
+                    placeholder="Type a message" />
+                <MdSend size={22} id="button-message-submit" />
+            </form>
+        </div>
+    )
+}
 
 export default Chat;
